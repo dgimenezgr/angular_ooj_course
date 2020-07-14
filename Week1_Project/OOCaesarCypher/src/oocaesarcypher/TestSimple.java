2c03217d29bb1a53c2672618154dcd5fbea2aa40
Daniel Giménez <danielgimenezgredilla@hotmail.com>
2020-07-14 17:47:40 +0200

    Finished test. TODO: BreakCipherTwoKeys key number.

---
+++ Week1_Project/OOCaesarCypher/src/oocaesarcypher/TestSimple.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oocaesarcypher;
+
+/**
+ *
+ * @author el_mo
+ */
+public class TestSimple {
+    public static void main(String[] args) {
+          Simple item = new Simple(3, "blue");     	
+          System.out.println(item);
+          System.out.println(item.mystery(5, "ho"));
+     }      
+}
